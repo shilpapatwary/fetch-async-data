4a158dd1a6b11be54083194416b5c3564d54f5d8
Shilpa Patwary <shilpa.patwary@sapient.com>
2019-10-28 11:10:51 +0530

    generating markup for the async data

--- js/userComments.js
+++ js/userComments.js
@@ -10,7 +10,26 @@ export default class UserComments {
   }
   getUserComments() {
     getUserComments().then(userData => {
-      console.log(userData);
+      this.paintUserComments(userData);
     });
+  }
+  getUserMarkup(data) {
+    const { address } = data.user,
+        { user } = data;
+    return `<div class="user-image"><span class="fa fa-user"></span></div><div class="user-details"><span class="user-name">${user.name}</span>
+    <span class="user-address"><span class="fa fa-address-card "></span>${address.suite}, ${address.street}, ${address.city}</span><span class="user-email"><span class="fa fa-envelope "></span>${user.email}</span>
+    <span class="user-phone"><span class="fa fa-phone "></span>${user.phone}</span><span class="user-web"><span class="fa fa-globe "></span>${user.website    }</span></div>`;
+  }
+
+  getCommentsMarkup(data) {
+    const { comments } = data,
+      commentsMarkup = comments.map(comment => {
+        return `<li ><div class="comment-title">${comment.title}</div><div class="comment-body">${comment.body}</div></li>`;
+      });
+    return commentsMarkup.join("");
+  }
+  paintUserComments(data) {
+    document.querySelector(".user").innerHTML = this.getUserMarkup(data);
+    document.querySelector(".user-comments").innerHTML = this.getCommentsMarkup(data);
   }
 }
